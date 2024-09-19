2d80b9b92c94089085b1028066789ae09ddde14d
root <root@DELL-7559>
2024-09-18 22:10:23 -0300

    Início do projeto

---
+++ src/main/java/Exercicio24.java
@@ -0,0 +1,33 @@
+
+// IMPORTS
+import java.util.Scanner;
+
+public class Exercicio24 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int s = 0;
+		int e = 0;
+		int cnt = 0;
+		int result = 0;
+
+		// COLLECT DATA
+		System.out.print("START : ");
+		s = sc.nextInt();
+		System.out.print("END : ");
+		e = sc.nextInt();
+
+		// OUTPUT
+		cnt = s;
+		while (cnt <= e) {
+			if (cnt % 2 == 0) {
+				System.out.println(cnt);
+			}
+			cnt++;
+		}
+
+	}
+}
