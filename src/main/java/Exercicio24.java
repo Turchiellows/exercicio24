ecad18290f287038eb1764a9e001980313c9d4cc
Turchiellows <>
2024-09-28 14:31:04 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio24.java
+++ src/main/java/Exercicio24.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 
 public class Exercicio24 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int s = 0;
-		int e = 0;
-		int cnt = 0;
-		int result = 0;
+      // DECLARATE
+      int s = 0;
+      int e = 0;
+      int cnt = 0;
+      int result = 0;
 
-		// COLLECT DATA
-		System.out.print("START : ");
-		s = sc.nextInt();
-		System.out.print("END : ");
-		e = sc.nextInt();
+      // COLLECT DATA
+      System.out.print("START : ");
+      s = sc.nextInt();
+      System.out.print("END : ");
+      e = sc.nextInt();
 
-		// OUTPUT
-		cnt = s;
-		while (cnt <= e) {
-			if (cnt % 2 == 0) {
-				System.out.println(cnt);
-			}
-			cnt++;
-		}
+      // OUTPUT
+      cnt = s;
+      while (cnt <= e) {
+         if (cnt % 2 == 0) {
+            System.out.println(cnt);
+         }
+         cnt++;
+      }
 
-	}
+   }
 }
